03329e9718f45773652a9005f788c1233f7847cc
lizezhi <1>
2023-12-15 16:03:42 +0800

    init

---
+++ miscellany/.vitepress/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    title: "Lshbosheth's Miscellany",
+    description: "Lshbosheth的杂类记录",
+    lang: 'zh-CN',
+    head: [
+        ['link', { rel: 'icon', href: 'favicon.ico', type: 'image/png' }]
+    ]
+}
